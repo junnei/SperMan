a71fb120ae389dd3200c9f3b2c871591c1e3fb93
seoyeon-1206 <ssy_1206@naver.com>
2024-08-25 10:04:42 +0900

    [Add] modify verified views

--- Sperman/app/apply/layout.tsx
+++ Sperman/app/apply/layout.tsx
@@ -5,7 +5,7 @@ export default function BlogLayout({
 }) {
   return (
     <section className="flex flex-col items-center justify-center">
-      <div className="inline-block max-w-lg text-center justify-center">
+      <div className="inline-block max-w-lg text-left justify-center">
         {children}
       </div>
     </section>
--- Sperman/app/verified/layout.tsx
+++ Sperman/app/verified/layout.tsx
@@ -4,8 +4,8 @@ export default function DocsLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="flex flex-col items-center justify-center">
-      <div className="inline-block max-w-lg text-center justify-center">
+    <section className="flex flex-col items-center justify-between h-full">
+      <div className="inline-block max-w-lg text-left justify-between h-full">
         {children}
       </div>
     </section>
